10949996014a09300f51e4d16040f33f52f35b50
cheater <195390+cheater@users.noreply.github.com>
2014-04-13 16:55:46 +0200

    Initial version of control-dsp. Includes generalized convolution.

---
+++ src/Control/DSP.hs
@@ -0,0 +1,29 @@
+module Control.DSP where
+
+-- | Generally, an impulse response should take an a, and its neighbours, and
+-- | return an a.
+-- |
+-- | Parameters:
+-- | previous - stuff before. A container of Maybe a.
+-- | current  - the sample right now, of type a.
+-- | further  - stuff after.  A container of Maybe a.
+-- |
+-- | Both "previous" and "further" use the same container.
+-- | The first element of each container should be the one nearest to the
+-- | current item (therefore, @previous@ is reversed in time).
+type IR t a = t (Maybe a) -> a -> t (Maybe a) -> a
+
+-- | For selecting the neighbours of a sample.
+type Taking t a = [a] -> t (Maybe a)
+
+convolve  :: IR t a -> Taking t a        -> [a] -> [a]
+convolve  ir take = convolve' ir take []
+
+convolve' :: IR t a -> Taking t a -> [a] -> [a] -> [a]
+convolve' _  _    _        []                = []
+convolve' ir take previous (current:further) = currentProcessed:furtherProcessed
+  where
+    currentProcessed = ir previousRelevant current furtherRelevant
+    furtherProcessed = convolve' ir take (current:previous) further
+    previousRelevant = take previous
+    furtherRelevant  = take further
